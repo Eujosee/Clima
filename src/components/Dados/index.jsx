94c07a0ba699d1b72e35504356eb5073102a92d7
Eujosee <josemotadossantosjunior@hotmail.com>
2023-05-01 23:13:09 -0300

    tentando arrumar responsivo em telas maiores

--- src/components/Dados/index.jsx
+++ src/components/Dados/index.jsx
@@ -4,99 +4,91 @@ import DadosSlider from "../DadosSlider"
 export default function Dados({ data }) {
   return (
     <div className="w-full max-h-1/2 p-6 pt-0 flex flex-col">
-      <h3 className="text-xl font-semibold mb-3 text-center md:text-start">Dados de hoje</h3>
-      <div className="hidden md:flex md:flex-col gap-y-6">
+      <h3 className="text-xl 2xl:text-4xl font-semibold mb-3 text-center md:text-start">Dados de hoje</h3>
+      <div className="hidden md:flex md:flex-col md:justify-end gap-y-6">
         <div className="grid grid-cols-2 lg:grid-cols-6 w-full gap-4">
-          <div className="flex flex-col lg:col-span-2 justify-center items-center bg-white p-4 rounded-2xl shadow-lg
-          transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200
+          <div className="flex flex-col lg:col-span-2 justify-center 2xl:justify-evenly items-center bg-white p-4 rounded-2xl shadow-lg 2xl:h-[25vh]
           ">
-            <p className="text-xl font-medium text-center">Úmidade do ar</p>
+            <p className="text-xl 2xl:text-3xl font-medium text-center">Úmidade do ar</p>
             <img
-              className="h-16 w-16"
+              className="h-16 w-16 2xl:h-32 2xl:w-32"
               src="/icons/humidity.png"
               alt="Uma gota de água com simbolo de porcentagem"
             />
-            <p className="text-gray-400 text-lg font-medium">
+            <p className="text-gray-400 text-lg 2xl:text-3xl font-medium">
               {data.main.humidity}%
             </p>
           </div>
           <div className="flex flex-col lg:col-span-2 justify-center items-center bg-white p-4 rounded-2xl shadow-lg
-          transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200
           ">
-            <p className="text-xl font-medium text-center">
+            <p className="text-xl font-medium text-center 2xl:text-3xl">
               Velocidade do vento
             </p>
-            <img className="h-16 w-16" src="/icons/vento.png" alt="Ventos" />
-            <p className="text-gray-400 font-medium text-lg">
+            <img className="h-16 w-16 2xl:h-32 2xl:w-32" src="/icons/vento.png" alt="Ventos" />
+            <p className="text-gray-400 font-medium text-lg 2xl:text-3xl">
               {data.wind.speed}km/h
             </p>
           </div>
           <div className="flex flex-col justify-center items-center bg-white p-4 rounded-2xl shadow-lg
-          transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200
           ">
-            <p className="text-xl font-medium">Máx.</p>
+            <p className="text-xl font-medium 2xl:text-3xl">Máx.</p>
             <img
-              className="h-16 w-16"
+              className="h-16 w-16 2xl:h-32 2xl:w-32"
               src="/icons/warm.png"
               alt="Termômetro no máximo"
             />
-            <p className="text-gray-400 font-medium text-lg">
+            <p className="text-gray-400 font-medium text-lg 2xl:text-3xl">
               {Math.trunc(data.main.temp_max)}°
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center bg-white p-4 rounded-2xl shadow-lg
-          transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200">
-            <p className="text-xl font-medium">Min.</p>
+          <div className="flex flex-col justify-center items-center bg-white p-4 rounded-2xl shadow-lg">
+            <p className="text-xl font-medium 2xl:text-3xl">Min.</p>
             <img
-              className="h-16 w-16"
+              className="h-16 w-16 2xl:h-32 2xl:w-32"
               src="/icons/cold.png"
               alt="Termômetro no minímo"
             />
-            <p className="text-gray-400 font-medium text-lg">
+            <p className="text-gray-400 font-medium text-lg 2xl:text-3xl">
               {Math.trunc(data.main.temp_min)}°
             </p>
           </div>
 
-          <div className="flex flex-col lg:col-span-2 justify-center items-center bg-white p-4 rounded-2xl shadow-lg
-          transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200">
-            <p className="text-xl font-medium">Pressão</p>
+          <div className="flex flex-col lg:col-span-2 justify-center items-center bg-white p-4 rounded-2xl shadow-lg 2xl:h-[25vh]">
+            <p className="text-xl font-medium 2xl:text-3xl" >Pressão</p>
             <img
-              className="h-16 w-16"
+              className="h-16 w-16 2xl:h-32 2xl:w-32"
               src="/icons/windy.png"
               alt="Nuvem com ventos"
             />
-            <p className="text-gray-400 font-medium text-lg">
+            <p className="text-gray-400 font-medium text-lg 2xl:text-3xl">
               {data.main.pressure} hPa
             </p>
           </div>
-          <div className="flex flex-col lg:col-span-2 justify-center items-center bg-white p-4 rounded-2xl shadow-lg
-          transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200">
-            <p className="text-xl font-medium">Nuvens</p>
-            <img className="h-16 w-16" src="/icons/nublado.png" alt="Nuvens" />
-            <p className="text-gray-400 font-medium text-lg">
+          <div className="flex flex-col lg:col-span-2 justify-center items-center bg-white p-4 rounded-2xl shadow-lg 2xl:h-[25vh]">
+            <p className="text-xl font-medium 2xl:text-3xl">Nuvens</p>
+            <img className="h-16 w-16 2xl:h-32 2xl:w-32" src="/icons/nublado.png" alt="Nuvens" />
+            <p className="text-gray-400 font-medium text-lg 2xl:text-3xl">
               {data.clouds.all}%
             </p>
           </div>
 
-          <div className="flex flex-col justify-center items-center bg-white p-4 rounded-2xl shadow-lg
-          transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200">
+          <div className="flex flex-col justify-center items-center bg-white p-4 rounded-2xl shadow-lg 2xl:h-[25vh]">
             <img
-              className="h-16 w-16"
+              className="h-16 w-16 2xl:h-32 2xl:w-32"
               src="/icons/sunrise.png"
               alt="Sol nascendo"
               />
-            <p className="text-gray-400 font-medium text-lg">
+            <p className="text-gray-400 font-medium text-lg 2xl:text-3xl">
               {moment.unix(data.sys.sunrise).format("HH:mm")}
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center bg-white p-4 rounded-2xl shadow-lg
-          transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200">
+          <div className="flex flex-col justify-center items-center bg-white p-4 rounded-2xl shadow-lg 2xl:h-[25vh]">
             <img
-              className="h-16 w-16"
+              className="h-16 w-16 2xl:h-32 2xl:w-32"
               src="/icons/sunset.png"
               alt="Sol se pondo"
               />
-            <p className="text-gray-400 font-medium text-lg">
+            <p className="text-gray-400 font-medium text-lg 2xl:text-3xl">
               {moment.unix(data.sys.sunset).format("HH:mm")}
             </p>
           </div>
--- src/components/PrevSemana/index.jsx
+++ src/components/PrevSemana/index.jsx
@@ -8,21 +8,20 @@ export default function PrevSemana({data}) {
 
   return (
     <div className="w-full p-6 flex flex-col">
-      <h1 className="text-xl font-semibold mb-3 text-center md:text-start">Próximos 5 dias</h1>
+      <h1 className="text-xl 2xl:text-4xl font-semibold mb-3 text-center md:text-start">Próximos 5 dias</h1>
       <div className="hidden md:flex md:flex-row md:flex-1 md:justify-evenly lg:justify-between gap-x-4 flex-wrap gap-4">
         {data.map((item, index) => {
                     return(
-                        <div key={index} className="flex flex-col items-center justify-center gap-y-2 bg-white p-4 rounded-2xl shadow-lg w-40
-                        transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200
+                        <div key={index} className="flex flex-col items-center justify-center 2xl:justify-evenly gap-y-2 bg-white p-4 rounded-2xl shadow-lg w-40 2xl:w-[10vw]  2xl:h-[25vh]
                         ">
-                            <p className="capitalize font-semibold text-lg">{moment(item.date).format('ddd')}.</p>
-                            <img className="h-16 w-16" 
+                            <p className="capitalize font-semibold text-lg 2xl:text-3xl">{moment(item.date).format('ddd')}.</p>
+                            <img className="h-16 w-16 2xl:h-28 2xl:w-28" 
                             src={`/icons/${item.icon}.png`}
                             alt={`${item.icon}`}
                             />
                             <div className="flex gap-1">
-                                <p className="font-medium">{Math.trunc(item.maxTemp)}°</p>
-                                <p className="text-gray-400 font-medium">/ {Math.trunc(item.minTemp)}°</p>
+                                <p className="font-medium 2xl:text-3xl">{Math.trunc(item.maxTemp)}°</p>
+                                <p className="text-gray-400 font-medium 2xl:text-3xl">/ {Math.trunc(item.minTemp)}°</p>
                             </div>
                         </div>
                     )
